9feae9b8c472f8804797f29f7449bcd6cd253421
Ali Yassine <84604171+ali-yasine@users.noreply.github.com>
2022-11-04 10:06:50 +0200

    refactored some code

--- DrayfusWagner.h
+++ DrayfusWagner.h
@@ -7,87 +7,92 @@
 #include <limits.h>
 
 unsigned int* DrayfusWagner(CsrGraph graph, unsigned int* terminals, unsigned int numTerminals, unsigned int* terminalMap) {
+
     unsigned int* apsp = floydWarshall(graph);
-    
-    unsigned int* DP = (unsigned int* ) calloc(graph.num_nodes * ( (1 << numTerminals) - 1), sizeof(unsigned int));
+
+    unsigned int total_subset_count = (1 << numTerminals) - 1;
+
+    unsigned int* DP = (unsigned int* ) calloc(graph.num_nodes * total_subset_count, sizeof(unsigned int));
     
     unsigned int* allSubsets = getSortedSubsets(numTerminals);
     
 
-    unsigned int totalSubsets = (1 << numTerminals) - 1;
-    unsigned int curr_Subset = 0;
-
     //init DP to INF
-    for(unsigned int i = 0; i < graph.num_nodes; ++i){
-        for(unsigned int j = 0; j < totalSubsets; ++j){
-            DP[i * totalSubsets + j] = UINT_MAX;
-        }
-    }
-
+    for(unsigned int i = 0; i < graph.num_nodes; ++i)
+        for(unsigned int j = 0; j < total_subset_count; ++j)
+            DP[i * total_subset_count + j] = UINT_MAX;
+    
     //handle singletons 
-    for(unsigned int vertex = 0; vertex < graph.num_nodes; ++vertex){
-        curr_Subset = 0;
-        for(unsigned int* subset = allSubsets; subset < allSubsets + numTerminals; ++subset){
+    for(unsigned int vertex = 0; vertex < graph.num_nodes; ++vertex) {
+        for(unsigned int subset = 0; subset < numTerminals; ++subset) {
+            
             //find index of 1 in subset
             unsigned int index = 0;
             for(unsigned int i = 0; i < numTerminals; ++i){
-                if (subset[i]){
+                if (allSubsets[subset * numTerminals + i]){
                     index = i;
                     break;
                 }
             }
-            DP[vertex * totalSubsets + curr_Subset] = apsp[vertex * graph.num_nodes + terminalMap[index]];
-            curr_Subset++;
+            
+            DP[vertex * total_subset_count + subset] = apsp[vertex * graph.num_nodes + terminalMap[index]];
         }
     }
+
+    //start at numTerminals since we already handled singletons
+    unsigned int curr_subset_index = numTerminals;
+
     //loop over subset sizes
     for(unsigned int k = 2; k < numTerminals; ++k) {
+        
         unsigned int numSubsets = choose(numTerminals, k);
         
         //loop over subsets
-        for (unsigned int subset = curr_Subset; subset <= curr_Subset + numSubsets ; ++subset ) {
+        for (unsigned int subset = curr_subset_index; subset <= curr_subset_index + numSubsets ; ++subset ) {
+            
             unsigned int* currSubset = allSubsets + (subset * numTerminals);
             
-            //unsigned int s_index = getSubsetIndex(currSubset, numTerminals, allSubsets);
-            unsigned int s_index = subset;
             
             for (unsigned int root = 0; root < graph.num_nodes; ++root) {
 
-                    unsigned int num_sub_subsets = (1 << k) - 1;
+                unsigned int num_sub_subsets = (1 << k) - 1;
 
-                    unsigned int* subSubsets = generateSubsets(currSubset, numTerminals);
-                    
-                    for(unsigned int subSubset = 0; subSubset < num_sub_subsets; ++subSubset) {
-                        unsigned int* curr_sub_subset = subSubsets + (subSubset * numTerminals);
-                        if (!equals(curr_sub_subset, currSubset, numTerminals)) {
+                unsigned int* subSubsets = generateSubsets(currSubset, numTerminals);
+                
+                unsigned int curr_cost = DP[root * total_subset_count + subset];
+                
+                for(unsigned int subSubset = 0; subSubset < num_sub_subsets; ++subSubset) {
 
-                            unsigned int ss_index = getSubsetIndex(curr_sub_subset, numTerminals, allSubsets);
-                            unsigned int* sMinusSS = setDifference(currSubset, curr_sub_subset, numTerminals);
-                            
+                    unsigned int* curr_sub_subset = subSubsets + (subSubset * numTerminals);
 
+                    //if they are equal then S/ss is empty so we skip
+                    if (!equals(curr_sub_subset, currSubset, numTerminals)) {
 
-                            unsigned int sMinusSS_index = getSubsetIndex(sMinusSS, numTerminals, allSubsets);
-                            unsigned int cost = DP[root * totalSubsets + s_index];
-                            for(unsigned int vertex = 0; vertex < graph.num_nodes; ++vertex){
-                                // DP[r, s] min= DP[v, ss] + DP[v, s / ss] + dist(r, v)
+                        unsigned int ss_index = getSubsetIndex(curr_sub_subset, numTerminals, allSubsets);
+                        unsigned int* sMinusSS = setDifference(currSubset, curr_sub_subset, numTerminals);        
 
-                                unsigned int v_to_sub_Subset = DP[vertex * totalSubsets + ss_index];
-                                unsigned int v_S_minusSS = DP[vertex * totalSubsets + sMinusSS_index]; 
-                                unsigned int root_to_V = apsp[root * graph.num_nodes + vertex]; 
+                        unsigned int sMinusSS_index = getSubsetIndex(sMinusSS, numTerminals, allSubsets);
 
-                                if (v_to_sub_Subset != UINT_MAX && v_S_minusSS != UINT_MAX && root_to_V != UINT_MAX) {
-                                    unsigned int sum = v_to_sub_Subset + v_S_minusSS + root_to_V;
-                                    if (sum < cost) {
-                                        DP[root * totalSubsets + s_index] = sum;
-                                        cost = sum;
-                                    }
+                        for(unsigned int vertex = 0; vertex < graph.num_nodes; ++vertex){
+                            // DP[r, s] min= DP[v, ss] + DP[v, s / ss] + dist(r, v)
+
+                            unsigned int v_to_sub_Subset = DP[vertex * total_subset_count + ss_index];
+                            unsigned int v_S_minusSS = DP[vertex * total_subset_count + sMinusSS_index]; 
+                            unsigned int root_to_V = apsp[root * graph.num_nodes + vertex]; 
+
+                            if (v_to_sub_Subset != UINT_MAX && v_S_minusSS != UINT_MAX && root_to_V != UINT_MAX) {
+                                unsigned int sum = v_to_sub_Subset + v_S_minusSS + root_to_V;
+                                if (sum < curr_cost) {
+                                    DP[root * total_subset_count + subset] = sum;
+                                    curr_cost = sum;
                                 }
                             }
                         }
                     }
+                }
             }
         }
-        curr_Subset += numSubsets;
+        curr_subset_index += numSubsets;
     }
     free(apsp);
     free(allSubsets);
--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include "DrayfusWagner.h"
-
+#include <chrono>
 #include <iostream>
+using namespace std;
+using namespace chrono;
+
 int main() {
-    //Testing on a graph of 7 vertices
+
+    //Testing on a graph of 10 vertices
     unsigned int numberOfVertices = 7;   
     unsigned int numberOfTeriminals = 3;
     unsigned int values[] =  {4, 8, 4, 2, 3, 8, 5, 7, 2, 6, 3, 5, 1, 1, 7, 1, 6, 1};
@@ -12,33 +16,35 @@ int main() {
     unsigned int terminals[] {2, 3, 5};
     unsigned int bitTerminals[] {1,1,1};
 
-    // Testing on a graph of 10 vertices
-    // unsigned int numberOfVertices = 10;   
-    // unsigned int numberOfTeriminals = 3;
-    // unsigned int values[] =  {4, 8, 4, 2, 3, 8, 5, 7, 2, 6, 3, 5, 1, 1, 3, 7, 1, 2, 4, 6, 1, 3, 3, 2, 4, 3};
-    // unsigned int col[] =     {1, 2, 0, 3, 4, 0, 4, 5, 1, 6, 1, 2, 5, 6, 7, 2, 4, 7, 8, 3, 4, 9, 4, 5, 5, 6};
-    // unsigned int rowPtr[] =  {0, 2, 5, 8, 10, 15, 19, 22, 24, 25, 26};
-    // unsigned int terminals[] {7, 8, 9};
-    // unsigned int bitTerminals[] {1,1,1};
-    // unsigned int numberOfTerminals = 3;
+    // Testing on a graph of 20 vertices
+    // unsigned int numberOfVertices = 20;   
+    // unsigned int values[] =  {29359, 16828, 2996, 14605, 12383, 5448, 11539, 17036, 28704, 4665, 12317, 1843, 30107, 12383, 6730, 15351, 3549, 19955, 13932, 22930, 13932, 2307, 22387, 6271, 15574, 16513, 13291, 4032, 18008, 29359, 5448, 27754, 14946, 6423, 2307, 18763, 27596, 11539, 6730, 22387, 30837, 11021, 24022, 19669, 8282, 15351, 4032, 27754, 26419, 18128, 24649, 17808, 14946, 16828, 18763, 26419, 30304, 17036, 3549, 6271, 30837, 32703, 20486, 14344, 2996, 28704, 22930, 11021, 29315, 12317, 15574, 24022, 18128, 19797, 15282, 19955, 16513, 6423, 29315, 20799, 1843, 13291, 19669, 32703, 20799, 23623, 14605, 4665, 30107, 8282, 24649, 30304, 20486, 19797, 23623, 6039, 18008, 27596, 17808, 14344, 15282, 6039};
+    // unsigned int col[] =     {7, 12, 14, 18, 3, 7, 9, 13, 14, 18, 15, 17, 18, 1, 9, 10, 13, 16, 5, 14, 4, 8, 9, 13, 15, 16, 17, 10, 19, 0, 1, 10, 11, 16, 5, 12, 19, 1, 3, 5, 13, 14, 15, 17, 18, 3, 6, 7, 12, 15, 18, 19, 7, 0, 8, 10, 18, 1, 3, 5, 9, 17, 18, 19, 0, 1, 4, 9, 16, 2, 5, 9, 10, 18, 19, 3, 5, 7, 14, 17, 2, 5, 9, 13, 16, 18, 0, 1, 2, 9, 10, 12, 13, 15, 17, 19, 6, 8, 10, 13, 15, 18};
+    // unsigned int rowPtr[] =  {0, 4, 10, 13, 18, 20, 27, 29, 34, 37, 45, 52, 53, 57, 64, 69, 75, 80, 86, 96, 102};
+    // unsigned int numberOfTerminals = 5;
+    // unsigned int terminals[] {3, 5, 7, 8, 9};
+    // unsigned int bitTerminals[] {1,1,1,1, 1};
 
     CsrGraph graph {
         numberOfVertices,
-        numberOfTeriminals,
+        numberOfTerminals,
         rowPtr,
         col,
         values
     };
 
-
-    std::cout << "starting\n";
+    //start
+    auto start = high_resolution_clock::now();
     unsigned int* result = DrayfusWagner(graph, bitTerminals, numberOfTerminals, terminals);
-    std::cout << "finished\n";
-    // Print the result
-    for(unsigned int vertex = 0; vertex < numberOfVertices; vertex++) {
-        for(unsigned int subset = 0; subset < (1 << numberOfTerminals) -1; subset++) {
-            std::cout << result[vertex * ((1 << numberOfTerminals )-1) + subset] << "\t";
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << "Time taken by function with " << numberOfVertices <<" vertices: "<< duration.count() << " nanoseconds" << endl;
+
+    cout << "Result: " << endl;
+    for (unsigned int root = 0; root < numberOfVertices; root++) {
+        for (unsigned int subset = 0; subset < (1 << numberOfTerminals) - 1; subset++) {
+            cout << result[root * ( (1 << numberOfTerminals) - 1 ) + subset] << "\t";
         }
-        std ::cout << std::endl;
+        cout << endl;
     }
 }
