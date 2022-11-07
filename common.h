0da1724d1a564adcd4d3a6018d3181e09821f4e3
imanelbanna <82767304+imanle@users.noreply.github.com>
2022-11-07 14:00:12 +0200

    header

--- common.h
+++ common.h
@@ -1,5 +1,5 @@
-#pragma once
-
+#ifndef __COMMON_H_
+#define __COMMON_H_
 #include "csr.h"
 
 CsrGraph* createEmptyCSRGraphOnGPU(unsigned int num_nodes, unsigned int num_edges);
@@ -9,3 +9,5 @@ void freeCSRGraphOnGPU(CsrGraph* graph);
 void DrayfusWagnerGPU(CsrGraph* graph_d, unsigned int numTerminals, unsigned int* terminals, unsigned int* DP, unsigned int* apsp);
 
 void copyCSRGraphToGPU(CsrGraph* graph, CsrGraph* graph_d);
+
+#endif
