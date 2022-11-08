d972ea6fa4fcd1007d79d031f4a0b01e077099b0
JamilMM39 <jamilmahmoud29@gmail.com>
2022-11-08 15:11:49 +0200

    fixed common.h

--- common.h
+++ common.h
@@ -6,7 +6,7 @@ CsrGraph* createEmptyCSRGraphOnGPU(unsigned int num_nodes, unsigned int num_edge
 
 void freeCSRGraphOnGPU(CsrGraph* graph);
 
-void DrayfusWagnerGPU(CsrGraph* graph_d, unsigned int numTerminals, unsigned int* terminals, unsigned int* DP, unsigned int* apsp);
+void DrayfusWagnerGPU(CsrGraph* graph, CsrGraph* graph_d, unsigned int numTerminals, unsigned int* terminals, unsigned int* DP, unsigned int* apsp);
 
 void copyCSRGraphToGPU(CsrGraph* graph, CsrGraph* graph_d);
 
